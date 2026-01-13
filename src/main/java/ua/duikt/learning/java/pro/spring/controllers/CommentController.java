3b81631b316462f858d2907c35b56155382df2ff
mykytasirobaba <mykyta.sirobaba@gmail.com>
2026-01-14 01:03:52 +0200

    Add description

--- src/main/java/ua/duikt/learning/java/pro/spring/controllers/CommentController.java
+++ src/main/java/ua/duikt/learning/java/pro/spring/controllers/CommentController.java
@@ -4,5 +4,6 @@ package ua.duikt.learning.java.pro.spring.controllers;
  * Created by Mykyta Sirobaba on 14.01.2026.
  * email mykyta.sirobaba@gmail.com
  */
+//TODO: Implements all necessary endpoints
 public class CommentController {
 }
