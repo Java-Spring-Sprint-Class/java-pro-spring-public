3948228b015cc83b9ac174b69122c28ed6dda39e
mykytasirobaba <mykyta.sirobaba@gmail.com>
2026-01-15 17:15:52 +0200

    ref: Add repositories CommentRepo and AttachmentRepo

--- src/main/java/ua/duikt/learning/java/pro/spring/repositories/AttachmentRepo.java
+++ src/main/java/ua/duikt/learning/java/pro/spring/repositories/AttachmentRepo.java
@@ -4,10 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.duikt.learning.java.pro.spring.entity.Attachment;
 
+import java.util.List;
+
 /**
  * Created by Mykyta Sirobaba on 14.01.2026.
  * email mykyta.sirobaba@gmail.com
  */
 @Repository
-public interface AttachmentRepo extends JpaRepository<Attachment, Integer> {
+public interface AttachmentRepo extends JpaRepository<Attachment, Long> {
+    List<Attachment> findAllByIssueId(Long issueId);
 }
--- src/main/java/ua/duikt/learning/java/pro/spring/repositories/CommentRepo.java
+++ src/main/java/ua/duikt/learning/java/pro/spring/repositories/CommentRepo.java
@@ -4,10 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.duikt.learning.java.pro.spring.entity.IssueComment;
 
+import java.util.List;
+
 /**
  * Created by Mykyta Sirobaba on 14.01.2026.
  * email mykyta.sirobaba@gmail.com
  */
 @Repository
-public interface CommentRepo extends JpaRepository<IssueComment, Integer> {
+public interface CommentRepo extends JpaRepository<IssueComment, Long> {
+    List<IssueComment> findAllByIssueId(Long issueId);
 }
